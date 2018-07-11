c19a8a2be0a9c6f1bd9209f4b3453e4d79b6a6ea
luohao <iluohao@qq.com>
2018-07-11 18:39:31 +0800

    help

--- _book/deploy.sh
+++ _book/deploy.sh
@@ -2,8 +2,8 @@ gitbook build
 
 git add .
 
-git commit -m "iluohao" 
+git commit -m "help" 
 
-git remote add origin 
+git remote add origin https://github.com/iluohao/help.git
 
 git push -u origin master
